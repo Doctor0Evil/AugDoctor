91de8bb4297b9d67fcc638dc350faa184fec465f
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-27 12:09:39 -0700

    Update main.rs

--- bci-ledger-service/src/main.rs
+++ bci-ledger-service/src/main.rs
@@ -1,5 +1,7 @@
 mod security;
+mod civic_profile;
 
+use civic_profile::CivicRewardProfile;
 use bci_bioledger_bridge::{BciEvent, BciLedgerOrchestrator};
 use biophysical_blockchain::{BioTokenState, HostEnvelope, InnerLedger, IdentityHeader};
 use bioscaleupgradeservice::neuralrope::NeuralRope;
@@ -14,6 +16,37 @@ use std::convert::Infallible;
 use std::sync::{Arc, Mutex};
 use tokio::net::TcpListener;
 use uuid::Uuid;
+
+#[derive(Clone)]
+struct AppState {
+    ledger: Arc<Mutex<InnerLedger>>,
+    rope: Arc<Mutex<NeuralRope>>,
+    civic_profile: Arc<CivicRewardProfile>,
+}
+
+fn init_ledger() -> InnerLedger { /* unchanged */ }
+
+fn init_civic_profile() -> CivicRewardProfile {
+    CivicRewardProfile::load_from_json(
+        "qpu/data/shards/profiles/civic-reward-profile.json",
+    )
+    .unwrap_or_else(|_| CivicRewardProfile {
+        multiplier_min: 0.0,
+        multiplier_max: 4.0,
+        default_multiplier: 1.0,
+        required_knowledge_factor: 0.60,
+        heroic_tags: HashSet::new(),
+        heroic_multiplier: 3.0,
+        good_tags: HashSet::new(),
+        good_multiplier: 1.5,
+        neutral_multiplier: 1.0,
+        disallowed_tags: HashSet::new(),
+        eco_bonus_enabled: false,
+        eco_low_threshold: 0.0,
+        eco_low_bonus: 1.0,
+    })
+}
+
 
 #[derive(Clone)]
 struct AppState {
