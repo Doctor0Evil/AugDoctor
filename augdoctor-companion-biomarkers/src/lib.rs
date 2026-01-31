09d4b691ef6a87cdb688fedae0948d56d8d0612d
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-31 01:47:03 -0700

    Update lib.rs

--- augdoctor-companion-biomarkers/src/lib.rs
+++ augdoctor-companion-biomarkers/src/lib.rs
@@ -1,13 +1,9 @@
 pub mod types;
 pub mod governor;
 pub mod recorder;
+pub mod neuralrope_helper;
 
-pub use types::{
-    CompanionBiomarkerSample,
-    CompanionBiomarkerPlane,
-    AssistantAutonomyProfile,
-    AssistantAutonomyDecision,
-    AssistantAutonomyReason,
-};
+pub use types::*;
 pub use governor::CompanionAutonomyGovernor;
 pub use recorder::{CompanionBiomarkerRecorder, BiomarkerAggregation};
+pub use neuralrope_helper::{AutonomyNeuralRopeHelper, AutonomyTraceAttributes, NeuralRopeLike};
