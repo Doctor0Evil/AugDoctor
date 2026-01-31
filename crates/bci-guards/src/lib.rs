c03d58d024474500d82edbb6ece6368729cd61e9
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-31 12:38:27 -0700

    Create lib.rs

---
+++ crates/bci-guards/src/lib.rs
@@ -0,0 +1,8 @@
+mod cognitive_load;
+mod manifest;
+mod muscle_safety;
+mod tests;
+
+pub use cognitive_load::CognitiveLoadEnvelopeV2;
+pub use manifest::generate_daily_manifest;
+pub use muscle_safety::MuscleSafetyEnvelopeV2;
