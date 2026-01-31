f44ba505fc1f876e650f8d3fc57e1b7c317e8b2e
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-31 13:06:47 -0700

    Create evolution_karmadecay.rs

---
+++ biophysical-blockchain/src/evolution_karmadecay.rs
@@ -0,0 +1,4 @@
+pub fn safe_decay_multiplier(aura: BiophysicalAura,
+                             domain: EvolutionDomain) -> DecayMultiplier {
+    // returns factor in 0.0..=1.0 and daily_cap_hit flag
+}
